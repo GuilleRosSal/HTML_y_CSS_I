237b4d4cf1d832313d454cb89e8eb7bf6aa628b6
Guillem Rosell Sales <guillerossal@uoc.edu>
2026-03-13 13:22:48 +0100

    Complete HTML and base CSS

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,12 @@
-const name = 'world';
-console.log(`Hello ${name}`);
+import { CookingPot, createIcons, SignalHigh, SignalLow, SignalMedium } from 'lucide';
+
+createIcons({
+  icons: {
+    CookingPot,
+    SignalLow,
+    SignalMedium,
+    SignalHigh,
+  },
+});
+
+//TODO: Añadir lógica para indicar la página activa en el menú de navegación.
